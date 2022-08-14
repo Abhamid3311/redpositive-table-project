504863d5b88b63c7a0754ef6d4c15cb314ca67e1
Hamid <www.abdulhamid3311@gmail.com>
2022-08-14 12:26:27 +0600

    added footer

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer class="footer footer-center p-4 bg-neutral text-neutral-content">
+            <div>
+                <p>Copyright © 2022 - All right reserved by Red Positive</p>
+            </div>
+        </footer>
     );
 };
 
